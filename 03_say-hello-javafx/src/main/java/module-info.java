11c4b53f60a27da5a04e890a550682c595fcb816
Kirill Koltunov <k.koltunov03@yandex.ru>
2022-03-17 13:54:16 +0300

    update pom

--- 03_say-hello-javafx/src/main/java/module-info.java
+++ 03_say-hello-javafx/src/main/java/module-info.java
@@ -2,8 +2,6 @@ module ru.hse.cs.sayhellojavafx {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires com.dlsc.formsfx;
-
     opens ru.hse.cs.sayhellojavafx to javafx.fxml;
     exports ru.hse.cs.sayhellojavafx;
 }
